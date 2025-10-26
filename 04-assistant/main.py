3949904dbd6bb7480a1d91c93afacda277297d93
xa0c <xa000c@gmail.com>
2025-10-26 20:45:38 +0200

    Refactor `main` function
    Move argument count check logic from `main` function to `parse_input`.
    Use single try-except block for the whole match-case block of commands.
    Enhance `parse_input` to return dict of command arguments and use them
    as kwargs, when passing to command handlers.
    Enhance `print_person` to handle output for all persons.

--- 04-assistant/main.py
+++ 04-assistant/main.py
@@ -31,49 +31,78 @@ specified person.
 NOTES:
     Person name can contain spaces."""
 
-MSG_BAD_ARG_COUNT = "ERROR: Wrong number of arguments. Use -h flag to read about command usage."
+MSG_BAD_ARG_COUNT = "Wrong number of arguments. Use -h flag to read about command usage."
 
-def parse_input(user_input: str) -> tuple[str, list[str]]:
+def parse_input(user_input: str) -> tuple[str, dict[str, str]]:
     """Parse string into a tuple of command name and its arguments.
 
     Args:
         user_input (str): Input string.
 
     Returns:
-        tuple[str, list]: Tuple of command name and it's arguments.
+        tuple[str, dict[str, str]]: Tuple of command name and a
+            dictionary of it's arguments.
+
+    Raises:
+        ValueError: If user input has wrong number of arguments.
     """
     if not user_input:
-        return "", []
+        return "", {}
+
     cmd, *args = user_input.strip().split()
+    match cmd:
+        case "add" | "change" | "delete":
+            if len(args) < 2:
+                raise ValueError(MSG_BAD_ARG_COUNT)
+            args = {"name": " ".join(args[:-1]), "value": args[-1]}
+        case "phone" | "email":
+            if len(args) < 1:
+                raise ValueError(MSG_BAD_ARG_COUNT)
+            args = {"name": " ".join(args)}
+        case "all":
+            args = {} if len(args) == 0 else {"name": " ".join(args)}
+
     return cmd.lower(), args
 
-def print_person(name: str, contacts: dict[str, set]):
+def print_person(name: str = None):
     """Print person's contacts as a 2-column table of phones and emails.
+
+    Prints single person if name is provided. Otherwise prints all.
 
     Args:
         name (str): Person's name.
-        contacts (dict[str, set]): Dictionary of sets for phones/emails.
+
+    Raises:
+        ValueError: If person doesn't exist.
     """
-    print('/' + '═' * 80 + '\\')
-    print('│ ' + f"Person: {name}".ljust(79) + '│')
-    print('├' + '─' * 80 + '┤')
-    print('│ ' + "Phones".center(30) + '│ ' + "Emails".center(47) + '│')
-    print('│' + '-' * 80 + '│')
-    phones = sorted(contacts["phones"])
-    emails = sorted(contacts["emails"])
-    length_diff = len(phones) - len(emails)
-    if length_diff > 0:
-        emails.extend([""] * length_diff)
-    elif length_diff < 0:
-        phones.extend([""] * abs(length_diff))
-    for phone, email in zip(phones, emails):
-        print('│ ' + str(phone).ljust(30) + '│ ' + email.ljust(47) + '│' )
-    print('└' + '─' * 80 + '┘')
+    persons = core.persons
+    if name:
+        if name not in core.persons:
+            raise ValueError("Specified person doesn't exist.")
+        persons = {name: core.persons[name]}
+
+    for name, contacts in persons.items():
+        print('/' + '═' * 80 + '\\')
+        print('│ ' + f"Person: {name}".ljust(79) + '│')
+        print('├' + '─' * 80 + '┤')
+        print('│ ' + "Phones".center(30) + '│ ' + "Emails".center(47) + '│')
+        print('│' + '-' * 80 + '│')
+        phones = sorted(contacts["phones"])
+        emails = sorted(contacts["emails"])
+        length_diff = len(phones) - len(emails)
+        if length_diff > 0:
+            emails.extend([""] * length_diff)
+        elif length_diff < 0:
+            phones.extend([""] * abs(length_diff))
+        for phone, email in zip(phones, emails):
+            print('│ ' + str(phone).ljust(30) + '│ ' + email.ljust(47) + '│' )
+        print('└' + '─' * 80 + '┘')
 
 def main():
     print(MSG_HELP)
 
     while True:
+        # Handle empty input, interrupts and parse errors
         try:
             cmd = input("> ")
             if not cmd:
@@ -82,90 +111,51 @@ def main():
         except (KeyboardInterrupt, EOFError):
             print("\nProgram interrupted by user.")
             break
+        except ValueError as e:
+            print("ERROR:", e, "Try again.")
+            continue
 
-        match cmd:
-            case "exit" | "close":
-                print("Exiting program. Good bye!")
-                break
-            case "hello":
-                print("Hello! How can I help you?")
-                continue
-            case "help":
-                print(MSG_HELP)
-                continue
-            case "add":
-                if len(args) < 2:
-                    print(MSG_BAD_ARG_COUNT)
-                    continue
-                try:
-                    result = core.add_contact(" ".join(args[:-1]), args[-1])
-                except ValueError as e:
-                    print("ERROR:", e, "Try again.")
-                    continue
-                print(f"{result.title()} contact added.")
-            case "change":
-                if len(args) < 2:
-                    print(MSG_BAD_ARG_COUNT)
-                    continue
-                try:
-                    result = core.change_contact(" ".join(args[:-1]), args[-1])
-                except ValueError as e:
-                    print("ERROR:", e, "Try again.")
-                    continue
-                print(f"Contact updated. All previous {result} contacts were rewritten.")
-            case "delete":
-                if len(args) < 2:
-                    print(MSG_BAD_ARG_COUNT)
-                    continue
-                try:
-                    result = core.delete_contact(" ".join(args[:-1]), args[-1])
-                except ValueError as e:
-                    print("ERROR:", e, "Try again.")
-                    continue
-                print(f"{result.title()} contact deleted.")
-            case "phone":
-                if len(args) < 1:
-                    print(MSG_BAD_ARG_COUNT)
-                    continue
-                name = " ".join(args)
-                try:
-                    result = core.show_phone(name)
-                except ValueError as e:
-                    print("ERROR:", e, "Try again.")
-                    continue
-                if result:
-                    print(f"List of phone numbers for {name}:")
-                    for phone in sorted(result): print(phone)
-                else:
-                    print(f"{name} doesn't have any phone numbers.")
-            case "email":
-                if len(args) < 1:
-                    print(MSG_BAD_ARG_COUNT)
-                    continue
-                name = " ".join(args)
-                try:
-                    result = core.show_email(name)
-                except ValueError as e:
-                    print("ERROR:", e, "Try again.")
-                    continue
-                if result:
-                    print(f"List of email addresses for {name}:")
-                    for email in sorted(result): print(email)
-                else:
-                    print(f"{name} doesn't have any email addresses.")
-            case "all":
-                if len(args) == 0:
-                    for name in core.persons:
-                        print_person(name, core.persons[name])
-                else:
-                    name = " ".join(args)
-                    if name not in core.persons:
-                        print("ERROR: Specified person doesn't exist. Try again.")
-                        continue
-                    print_person(name, core.persons[name])
-            case _:
-                print("ERROR: Unknown command. Try again.")
-                continue
+        # Handle commands
+        try:
+            match cmd:
+                case "exit" | "close":
+                    print("Exiting program. Good bye!")
+                    break
+                case "hello":
+                    print("Hello! How can I help you?")
+                case "help":
+                    print(MSG_HELP)
+                case "add":
+                    result = core.add_contact(**args)
+                    print(f"{result.title()} contact added.")
+                case "change":
+                    result = core.change_contact(**args)
+                    print(f"Contact updated. All previous {result} contacts were rewritten.")
+                case "delete":
+                    result = core.delete_contact(**args)
+                    print(f"{result.title()} contact deleted.")
+                case "phone":
+                    result = core.show_phone(**args)
+                    if result:
+                        print(f"List of phone numbers for {args["name"]}:")
+                        for phone in sorted(result):
+                            print(phone)
+                    else:
+                        print(f"{args["name"]} doesn't have any phone numbers.")
+                case "email":
+                    result = core.show_email(**args)
+                    if result:
+                        print(f"List of email addresses for {args["name"]}:")
+                        for email in sorted(result):
+                            print(email)
+                    else:
+                        print(f"{args["name"]} doesn't have any email addresses.")
+                case "all":
+                    print_person(**args)
+                case _:
+                    print("ERROR: Unknown command. Try again.")
+        except ValueError as e:
+            print("ERROR:", e, "Try again.")
 
 if __name__ == "__main__":
     main()
